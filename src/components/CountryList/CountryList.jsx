ab89945002838b8c934e1c48870f6c75749ad4ef
Tetyana Gogulya <cheburashka077@ukr.net>
2024-06-09 13:03:26 +0300

    adduseParams

--- src/components/CountryList/CountryList.jsx
+++ src/components/CountryList/CountryList.jsx
@@ -6,7 +6,7 @@ export const CountryList = ({ list }) => {
     <Grid>
       {list.map(item => (
         <GridItem key={item.id}>
-          <Link>
+          <Link to={`/country/${item.id}`}>
             <img src={item.flag} alt={item.name} />
           </Link>
         </GridItem>
--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,6 +1,9 @@
 import { Container, Heading, Section } from 'components';
+import { useParams } from 'react-router-dom';
 
 export const Country = () => {
+  const { countryId } = useParams();
+  console.log(countryId);
   return (
     <Section>
       <Container>
